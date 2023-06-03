2684a1b8b295dc129e2f0af40fd8666ef75053d7
“selena” <selena606352000@gmail.com>
2023-06-03 22:46:03 +0800

    修改” git commit -m 助教意見修改”

--- js/all.js
+++ js/all.js
@@ -1,3 +1,24 @@
+const swiper = new Swiper(".swiper", {
+  // 分頁、左右箭頭、滾動條若有使用則必需設定
+  // 分頁
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 24,
+    },
+    1025: {
+      slidesPerView: 3,
+      slidesPerGroup: 1,
+      spaceBetween: 24,
+    },
+  },
+});
+
 //BackToTop按鈕，點選後移至最上面
 $(document).ready(function () {
   $(".backtotop").click(function (e) {
@@ -11,54 +32,26 @@ $(document).ready(function () {
   });
 });
 //問題集按鈕
-$(document).ready(function () {
-  $(".option1__content").hide();
-  $(".option1").click(function (e) {
-    e.preventDefault();
-    $(".icon-add1").toggleClass("opacity");
-    $(".option1__content").slideToggle();
-  });
-});
-$(document).ready(function () {
-  $(".option2__content").hide();
-  $(".option2").click(function (e) {
-    e.preventDefault();
-    $(".icon-add2").toggleClass("opacity");
-    $(".option2__content").slideToggle();
-  });
-});
-$(document).ready(function () {
-  $(".option3__content").hide();
-  $(".option3").click(function (e) {
-    e.preventDefault();
-    $(".icon-add3").toggleClass("opacity");
-    $(".option3__content").slideToggle();
-  });
-});
-$(document).ready(function () {
-  $(".option4__content").hide();
-  $(".option4").click(function (e) {
-    e.preventDefault();
-    $(".icon-add4").toggleClass("opacity");
-    $(".option4__content").slideToggle();
-  });
-});
-$(document).ready(function () {
-  $(".option5__content").hide();
-  $(".option5").click(function (e) {
-    e.preventDefault();
-    $(".icon-add5").toggleClass("opacity");
-    $(".option5__content").slideToggle();
+$(function () {
+  $(".option__content").hide();
+  $(".icon-remove").addClass("opacity");
+  $(".option").click(function () {
+    $(".option__content").slideUp();
+    $(".icon-remove").addClass("opacity");
+    if ($(".option__content", this).css("display") == "none") {
+      $(".option__content", this).slideDown();
+      $(".icon-remove", this).removeClass("opacity");
+    }
   });
 });
 //漢堡選單按鈕
 $(document).ready(function () {
-  $(".nav-icon__list").toggle();
-  $(".icon-menu").click(function (e) {
+  $(".nav-icon__list").hide();
+  $(".nav-icon__menu ").click(function (e) {
     e.preventDefault();
-    $(".icon-menu").toggleClass("opacity");
-    $("main,.footer-link").toggle();
-    $(".nav-icon__list").toggle();
+    $(".nav-icon__menu ").toggleClass("opacity");
+    $("main,.footer-link").slideToggle();
+    $(".nav-icon__list").slideToggle();
   });
 });
 //篩選按鈕
